1ad29a9edc326414edbb389f5855dfc21a8bc649
aaronlizaschool1-rgb <aaronlizaschool1@gmail.com>
2026-06-23 20:37:53 +0800

    DatabaseInitializer

---
+++ EntertainmentHub/DatabaseInitializerModule.vb
@@ -0,0 +1,35 @@
+﻿Module DatabaseInitializerModule
+    Public Sub SeedDatabase()
+
+        SeedMembershipLevels()
+        SeedRoles()
+        SeedEntertainmentTypes()
+        SeedEntertainmentTiers()
+
+    End Sub
+
+    Private Sub SeedMembershipLevels()
+
+        ' Check and insert Basic, Premium, VIP
+
+    End Sub
+
+    Private Sub SeedRoles()
+
+        ' Check and insert Admin, Cashier, Staff
+
+    End Sub
+
+    Private Sub SeedEntertainmentTypes()
+
+        ' Check and insert PC, Console
+
+    End Sub
+
+    Private Sub SeedEntertainmentTiers()
+
+        ' Check and insert Standard PC, High-End PC, PS5
+
+    End Sub
+
+End Module
