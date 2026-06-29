6de483ab0877a48030d2822c5f63d3d44a96c30e
Abenad1123 <103361048+Abenad1123@users.noreply.github.com>
2026-06-29 17:29:12 +0800

    Add cross-grid filtering and unified profile updates
    - Built Membership and Status dropdowns to filter the customer and account grids simultaneously.
    - Set up click events so selecting a row instantly populates the edit textboxes.
    - Grouped the Update logic into a single safe transaction so customer info, account status, and logins update together without errors.
    - Added smart password handling: updating a profile won't break the user's current password if the password box is left blank.

--- EntertainmentHub/Forms/Admin Forms/UserManagement Forms/UpdateUser.Designer.vb
+++ EntertainmentHub/Forms/Admin Forms/UserManagement Forms/UpdateUser.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class UpdateUser
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,28 +20,341 @@ Partial Class UpdateUser
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(UpdateUser))
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.txtboxPassword = New System.Windows.Forms.TextBox()
+        Me.txtboxUsername = New System.Windows.Forms.TextBox()
+        Me.txtboxContactNum = New System.Windows.Forms.TextBox()
+        Me.txtboxLastName = New System.Windows.Forms.TextBox()
+        Me.txtboxFirstName = New System.Windows.Forms.TextBox()
+        Me.lblRole = New System.Windows.Forms.Label()
+        Me.cmbboxMembership = New System.Windows.Forms.ComboBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.txtboxSearchBox = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.cmbboxEditStatus = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.cmbboxEditMembership = New System.Windows.Forms.ComboBox()
+        Me.cmbboxStatus = New System.Windows.Forms.ComboBox()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtboxEmail = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button3
         '
+        Me.Button3.BackColor = System.Drawing.Color.White
         Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button3.Location = New System.Drawing.Point(539, 331)
+        Me.Button3.Location = New System.Drawing.Point(13, 730)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(123, 40)
-        Me.Button3.TabIndex = 18
+        Me.Button3.TabIndex = 19
         Me.Button3.Text = "Go back"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView2.Location = New System.Drawing.Point(13, 357)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.RowHeadersWidth = 51
+        Me.DataGridView2.RowTemplate.Height = 24
+        Me.DataGridView2.Size = New System.Drawing.Size(574, 328)
+        Me.DataGridView2.TabIndex = 21
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(23, 259)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(154, 25)
+        Me.Label7.TabIndex = 21
+        Me.Label7.Text = "Contact Number"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(79, 496)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(98, 25)
+        Me.Label6.TabIndex = 20
+        Me.Label6.Text = "Password"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(71, 446)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(102, 25)
+        Me.Label4.TabIndex = 18
+        Me.Label4.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(71, 192)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(106, 25)
+        Me.Label2.TabIndex = 16
+        Me.Label2.Text = "Last Name"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(71, 156)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 25)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "First Name"
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.BackColor = System.Drawing.Color.White
+        Me.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnUpdate.Location = New System.Drawing.Point(222, 592)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(238, 49)
+        Me.btnUpdate.TabIndex = 14
+        Me.btnUpdate.Text = "UPDATE"
+        Me.btnUpdate.UseVisualStyleBackColor = False
+        '
+        'txtboxPassword
+        '
+        Me.txtboxPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtboxPassword.Location = New System.Drawing.Point(183, 494)
+        Me.txtboxPassword.Name = "txtboxPassword"
+        Me.txtboxPassword.Size = New System.Drawing.Size(339, 30)
+        Me.txtboxPassword.TabIndex = 13
+        '
+        'txtboxUsername
+        '
+        Me.txtboxUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtboxUsername.Location = New System.Drawing.Point(183, 444)
+        Me.txtboxUsername.Name = "txtboxUsername"
+        Me.txtboxUsername.Size = New System.Drawing.Size(339, 30)
+        Me.txtboxUsername.TabIndex = 12
+        '
+        'txtboxContactNum
+        '
+        Me.txtboxContactNum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtboxContactNum.Location = New System.Drawing.Point(183, 254)
+        Me.txtboxContactNum.Name = "txtboxContactNum"
+        Me.txtboxContactNum.Size = New System.Drawing.Size(339, 30)
+        Me.txtboxContactNum.TabIndex = 10
+        '
+        'txtboxLastName
+        '
+        Me.txtboxLastName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtboxLastName.Location = New System.Drawing.Point(183, 190)
+        Me.txtboxLastName.Name = "txtboxLastName"
+        Me.txtboxLastName.Size = New System.Drawing.Size(339, 30)
+        Me.txtboxLastName.TabIndex = 8
+        '
+        'txtboxFirstName
+        '
+        Me.txtboxFirstName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtboxFirstName.Location = New System.Drawing.Point(183, 154)
+        Me.txtboxFirstName.Name = "txtboxFirstName"
+        Me.txtboxFirstName.Size = New System.Drawing.Size(339, 30)
+        Me.txtboxFirstName.TabIndex = 7
+        '
+        'lblRole
+        '
+        Me.lblRole.AutoSize = True
+        Me.lblRole.Location = New System.Drawing.Point(19, 55)
+        Me.lblRole.Name = "lblRole"
+        Me.lblRole.Size = New System.Drawing.Size(120, 25)
+        Me.lblRole.TabIndex = 6
+        Me.lblRole.Text = "Membership"
+        '
+        'cmbboxMembership
+        '
+        Me.cmbboxMembership.FormattingEnabled = True
+        Me.cmbboxMembership.Location = New System.Drawing.Point(145, 49)
+        Me.cmbboxMembership.Name = "cmbboxMembership"
+        Me.cmbboxMembership.Size = New System.Drawing.Size(121, 33)
+        Me.cmbboxMembership.TabIndex = 5
+        '
+        'btnSearch
+        '
+        Me.btnSearch.BackColor = System.Drawing.Color.White
+        Me.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSearch.Location = New System.Drawing.Point(373, 13)
+        Me.btnSearch.Margin = New System.Windows.Forms.Padding(0)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(132, 30)
+        Me.btnSearch.TabIndex = 4
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = False
+        '
+        'txtboxSearchBox
+        '
+        Me.txtboxSearchBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtboxSearchBox.Location = New System.Drawing.Point(28, 13)
+        Me.txtboxSearchBox.Name = "txtboxSearchBox"
+        Me.txtboxSearchBox.Size = New System.Drawing.Size(325, 30)
+        Me.txtboxSearchBox.TabIndex = 3
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(13, 13)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(574, 328)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 5
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Button3, 1, 5)
+        Me.TableLayoutPanel1.Controls.Add(Me.DataGridView1, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Panel1, 3, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.DataGridView2, 1, 3)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Margin = New System.Windows.Forms.Padding(0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 6
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.5!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.5!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(1200, 808)
+        Me.TableLayoutPanel1.TabIndex = 22
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.cmbboxEditStatus)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label8)
+        Me.Panel1.Controls.Add(Me.cmbboxEditMembership)
+        Me.Panel1.Controls.Add(Me.cmbboxStatus)
+        Me.Panel1.Controls.Add(Me.lblStatus)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.txtboxEmail)
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.btnUpdate)
+        Me.Panel1.Controls.Add(Me.txtboxPassword)
+        Me.Panel1.Controls.Add(Me.txtboxUsername)
+        Me.Panel1.Controls.Add(Me.txtboxContactNum)
+        Me.Panel1.Controls.Add(Me.txtboxLastName)
+        Me.Panel1.Controls.Add(Me.txtboxFirstName)
+        Me.Panel1.Controls.Add(Me.lblRole)
+        Me.Panel1.Controls.Add(Me.cmbboxMembership)
+        Me.Panel1.Controls.Add(Me.btnSearch)
+        Me.Panel1.Controls.Add(Me.txtboxSearchBox)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(613, 13)
+        Me.Panel1.Name = "Panel1"
+        Me.TableLayoutPanel1.SetRowSpan(Me.Panel1, 3)
+        Me.Panel1.Size = New System.Drawing.Size(574, 672)
+        Me.Panel1.TabIndex = 20
+        '
+        'cmbboxEditStatus
+        '
+        Me.cmbboxEditStatus.FormattingEnabled = True
+        Me.cmbboxEditStatus.Location = New System.Drawing.Point(401, 335)
+        Me.cmbboxEditStatus.Name = "cmbboxEditStatus"
+        Me.cmbboxEditStatus.Size = New System.Drawing.Size(121, 33)
+        Me.cmbboxEditStatus.TabIndex = 31
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(327, 341)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(68, 25)
+        Me.Label5.TabIndex = 30
+        Me.Label5.Text = "Status"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(57, 341)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(120, 25)
+        Me.Label8.TabIndex = 29
+        Me.Label8.Text = "Membership"
+        '
+        'cmbboxEditMembership
+        '
+        Me.cmbboxEditMembership.FormattingEnabled = True
+        Me.cmbboxEditMembership.Location = New System.Drawing.Point(183, 335)
+        Me.cmbboxEditMembership.Name = "cmbboxEditMembership"
+        Me.cmbboxEditMembership.Size = New System.Drawing.Size(121, 33)
+        Me.cmbboxEditMembership.TabIndex = 28
+        '
+        'cmbboxStatus
+        '
+        Me.cmbboxStatus.FormattingEnabled = True
+        Me.cmbboxStatus.Location = New System.Drawing.Point(145, 96)
+        Me.cmbboxStatus.Name = "cmbboxStatus"
+        Me.cmbboxStatus.Size = New System.Drawing.Size(121, 33)
+        Me.cmbboxStatus.TabIndex = 27
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(71, 96)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(68, 25)
+        Me.lblStatus.TabIndex = 26
+        Me.lblStatus.Text = "Status"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(113, 292)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 25)
+        Me.Label3.TabIndex = 25
+        Me.Label3.Text = "Email"
+        '
+        'txtboxEmail
+        '
+        Me.txtboxEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtboxEmail.Location = New System.Drawing.Point(183, 290)
+        Me.txtboxEmail.Name = "txtboxEmail"
+        Me.txtboxEmail.Size = New System.Drawing.Size(339, 30)
+        Me.txtboxEmail.TabIndex = 24
         '
         'UpdateUser
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1200, 703)
-        Me.Controls.Add(Me.Button3)
+        Me.ClientSize = New System.Drawing.Size(1200, 808)
+        Me.Controls.Add(Me.TableLayoutPanel1)
         Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
@@ -49,9 +362,41 @@ Partial Class UpdateUser
         Me.Name = "UpdateUser"
         Me.Text = "Admin"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents Button3 As Button
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents txtboxPassword As TextBox
+    Friend WithEvents txtboxUsername As TextBox
+    Friend WithEvents txtboxContactNum As TextBox
+    Friend WithEvents txtboxLastName As TextBox
+    Friend WithEvents txtboxFirstName As TextBox
+    Friend WithEvents lblRole As Label
+    Friend WithEvents cmbboxMembership As ComboBox
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents txtboxSearchBox As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents cmbboxStatus As ComboBox
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtboxEmail As TextBox
+    Friend WithEvents cmbboxEditStatus As ComboBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents cmbboxEditMembership As ComboBox
 End Class
