e53133c5c9bc41868bbdee13f253d5611fdfdd26
aaronlizaschool1-rgb <aaronlizaschool1@gmail.com>
2026-06-29 17:31:30 +0800

    Commit_to_push

--- EntertainmentHub/Forms/Admin Forms/EntertainmentManagement Forms/EntertainmentConfiguration.vb
+++ EntertainmentHub/Forms/Admin Forms/EntertainmentManagement Forms/EntertainmentConfiguration.vb
@@ -1,3 +1,29 @@
 ﻿Public Class EntertainmentConfiguration
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
 
+    End Sub
+
+    Private Sub EntertainmentConfiguration_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
 End Class
